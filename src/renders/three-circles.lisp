7754b3add9bed0dbb29f93f59e13ed7876b467a3
Theodor Thornhill <theothornhill@pm.me>
2020-03-23 15:09:40 +0100

    Add three-circles.lisp to renders

---
+++ src/renders/three-circles.lisp
@@ -0,0 +1,78 @@
+(in-package #:lisp-tracer-renders)
+
+(defparameter *pi* (coerce pi 'single-float))
+
+(defparameter *scene-floor*
+  (make-sphere
+   :transform (scaling 10.0 0.01 10.0)
+   :material (make-material
+              :col (make-color :red 1.0 :green 0.9 :blue 0.9)
+              :specular 0.0)))
+
+(defparameter *scene-left-wall*
+  (make-sphere
+   :transform (transform-object
+            (scaling 10.0 0.01 10.0)
+            (rotation-x (div *pi* 2.0))
+            (rotation-y (neg (div *pi* 4.0)))
+            (translation 0.0 0.0 5.0))))
+
+(defparameter *scene-right-wall*
+  (make-sphere
+   :transform (transform-object
+            (scaling 10.0 0.01 10.0)
+            (rotation-x (div *pi* 2.0))
+            (rotation-y (div *pi* 4.0))
+            (translation 0.0 0.0 5.0))))
+
+(defparameter *middle*
+  (make-sphere
+   :transform (translation -0.5 1.0 0.5)
+   :material (make-material
+              :col (make-color :red 0.1 :green 1.0 :blue 0.5)
+              :specular 0.7
+              :diffuse 0.3)))
+
+(defparameter *right*
+  (make-sphere
+   :transform (mult (scaling 0.5 0.5 0.5)
+                    (translation 2.5 1.0 -0.5))
+   :material (make-material
+              :col (make-color :red 0.5 :green 1.0 :blue 0.1)
+              :specular 0.7
+              :diffuse 0.3)))
+
+(defparameter *left*
+  (make-sphere
+   :transform (mult (translation -1.5 0.33 -0.75)
+                    (scaling 0.33 0.33 0.33))
+   :material (make-material
+              :col (make-color :red 1.0 :green 0.8 :blue 0.1)
+              :specular 0.7
+              :diffuse 0.3)))
+
+(defparameter *world*
+  (make-world
+   :objects (list *scene-floor*
+                  *scene-left-wall*
+                  *scene-right-wall*
+                  *middle*
+                  *right*
+                  *left*)
+   :light (point-light
+           (make-point -1000.0 1000.0 -1000.0)
+           (make-color :red 1.0
+                       :green 1.0
+                       :blue 1.0))))
+
+(defparameter *camera*
+  (create-camera
+   1000
+   500
+   (div *pi* 3.0)
+   (view-transform (make-point 0.0 1.5 -5.0)
+                   (make-point 0.0 1.0 0.0)
+                   (make-vec 0.0 1.0 0.0))))
+
+(defparameter *canvas* (render *camera* *world*))
+(canvas-to-ppm *canvas*)
