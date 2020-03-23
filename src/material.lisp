4b6d093c85d23523eb2874efbccfe7d7917cc705
Theodor Thornhill <theothornhill@pm.me>
2020-03-23 21:17:17 +0100

    Cleanup lighting function

--- src/material.lisp
+++ src/material.lisp
@@ -12,30 +12,22 @@
   (declare (optimize (speed 3) (safety 0)))
   (let* ((effective-color (mult (material-col material) (light-intensity light)))
          (lightv (normalize (sub (light-position light) point)))
-         (material-ambient (mult effective-color (material-ambient material)))
-         (light-dot-normal (dot lightv normalv))
-         (black (make-color)))
-    (if shadowed?
-        material-ambient
-        (let (material-diffuse material-specular)
-          (declare (type single-float light-dot-normal))
-          (if (< light-dot-normal 0.0)
-              (progn
-                (setf material-diffuse black)
-                (setf material-specular black))
-              (progn
-                (setf material-diffuse
-                      (mult effective-color
-                            (mult (material-diffuse material)
-                                  light-dot-normal)))
-                (let ((reflect-dot-eye (dot (reflect (neg lightv) normalv) eyev)))
-                  (declare (type single-float reflect-dot-eye))
-                  (if (<= reflect-dot-eye 0)
-                      (setf material-specular black)
-                      (setf material-specular
-                            (mult (light-intensity light)
-                                  (mult
-                                   (material-specular material)
-                                   (expt reflect-dot-eye
-                                         (material-shininess material)))))))))
-          (add material-ambient (add material-diffuse material-specular))))))
+         (ambient (mult effective-color (material-ambient material)))
+         (light-dot-normal (dot lightv normalv)))
+    (declare (type single-float light-dot-normal))
+    (if (or (< light-dot-normal 0.0) shadowed?)
+        ambient
+        (let ((diffuse (mult effective-color
+                             (mult (material-diffuse material)
+                                   light-dot-normal)))
+              (reflect-dot-eye (dot (reflect (neg lightv) normalv) eyev)))
+          (declare (type single-float reflect-dot-eye))
+          (if (<= reflect-dot-eye 0)
+              (add ambient diffuse)
+              (let ((specular
+                      (mult (light-intensity light)
+                            (mult
+                             (material-specular material)
+                             (expt reflect-dot-eye
+                                   (material-shininess material))))))
+                (add ambient (add diffuse specular))))))))
