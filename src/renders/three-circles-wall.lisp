8cde2b7fc3cf59410e7acfcc0c6be583b599a921
Theodor Thornhill <theothornhill@pm.me>
2020-04-04 21:25:59 +0200

    Use new scaling and translation functions

--- src/renders/three-circles-wall.lisp
+++ src/renders/three-circles-wall.lisp
@@ -2,28 +2,35 @@
 
 (defparameter *backdrop*
   (make-plane
-   :transform (mult (rotation-x (/ pi 2))
-                    (translation 0.0 1.5 0.0))
    :material (make-material
               :color (make-color :red 1.0 :green 0.9 :blue 0.9)
-              :specular 0.0)))
+              :specular 0.0
+              :pattern (checkers-pattern (make-color :red 0.0 :green 0.0 :blue 0.0)
+                                         (make-color :red 1.0 :green 1.0 :blue 1.0)
+                                         (translation-y 0.1)))))
 
 (defparameter *middle*
   (make-sphere
    :transform (translation -0.5 1.0 0.5)
    :material (make-material
-              :color (make-color :red 0.1 :green 1.0 :blue 0.5)
               :specular 0.3
-              :diffuse 0.7)))
+              :diffuse 0.7
+              :pattern (ring-pattern (make-color :red 0.2 :green 0.5 :blue 0.2)
+                                     (make-color :red 1.0 :green 0.8 :blue 1.0)
+                                     (mult (translation 1.5 1.5 1.5)
+                                           (scaling 0.2 0.2 0.2))))))
 
 (defparameter *right*
   (make-sphere
    :transform (mult (scaling 0.5 0.5 0.5)
                     (translation 2.5 1.0 -0.5))
    :material (make-material
-              :color (make-color :red 0.5 :green 1.0 :blue 0.1)
               :specular 0.3
-              :diffuse 0.7)))
+              :diffuse 0.7
+              :pattern (gradient-pattern (make-color :red 1.0 :green 1.0 :blue 0.1)
+                                         (make-color :red 0.1 :green 0.8 :blue 0.7)
+                                         (mult (translation-x -1.0)
+                                               (scaling-x 2.0))))))
 
 (defparameter *left*
   (make-sphere
